f14da91bdce5c81255fbdffb528f0b0bd0b10a59
Matt Tyler <tylma141@student.otago.ac.nz>
2023-02-17 21:52:20 +1300

    Simple case working
    Using figlet's hello world, got a simple text replace with comment out running.

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const figlet = require('figlet');
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,27 +12,45 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ascii-titles" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand("ascii-titles.helloWorld",
+        function () {
+            // The code you place here will be executed every time your command is executed
+            const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ascii-titles.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+            if (editor) {
+                const document = editor.document;
+                const selection = editor.selection;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ASCII Titles!');
-	});
+                // Get the word within the selection
+                var title = document.getText(selection);
 
-	context.subscriptions.push(disposable);
+                figlet(title, function (err, data) {
+                    if (err) {
+                        console.log('Something went wrong...');
+                        console.dir(err);
+                        return;
+                    }
+                    title = data;
+                });
+
+                editor.edit((editBuilder) => {
+                    editBuilder.replace(selection, title);
+                });
+                vscode.commands.executeCommand('editor.action.addCommentLine');
+            }
+        }
+    );
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate,
+};
